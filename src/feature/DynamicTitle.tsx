a69821af24304db6860c194b350d76cdce87ae1a
DiNg <dinhquangnguyen1997@gmail.com>
2024-11-02 08:10:29 +0700

    fix: 🐛 add prettier package

--- src/feature/DynamicTitle.tsx
+++ src/feature/DynamicTitle.tsx
@@ -1,12 +1,10 @@
 "use client";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import { findTitle } from "../../utils/findTitle";
 
 export default function DynamicTitle() {
   const [title, setTitle] = useState("xxx");
   const pathname = usePathname();
-  console.log('pathname :>> ', pathname);
 
   useEffect(() => {
     switch (pathname) {
